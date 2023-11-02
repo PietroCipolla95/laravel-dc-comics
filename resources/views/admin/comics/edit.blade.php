e1368bbf3ec7cdb24d3c160ef3e04dcd11cdb301
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-11-02 19:08:10 +0100

    add edit function

---
+++ resources/views/admin/comics/edit.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.admin')
+
+@section('main')
+    <div class="container my-5">
+
+
+        <form action="{{ route('comics.update', $comic) }}" method="POST" enctype="multipart/form-data">
+
+
+            @csrf
+            @method('PUT')
+            <div class="mb-3">
+                <label for="title" class="form-label">Title</label>
+                <input type="text" class="form-control" name="title" id="title" aria-describedby="helpId"
+                    placeholder="Acolyte Eco Battle staff" value="{{ $comic->title }}">
+                <small id="titleHelper" class="form-text text-muted">Type the title here</small>
+            </div>
+
+            <div class="mb-3">
+                <label for="price" class="form-label">Price</label>
+                <input type="number" step="0.01" class="form-control" name="price" id="price"
+                    aria-describedby="helpId" placeholder="99.99" value="{{ $comic->price }}">
+                <small id="priceHelper" class="form-text text-muted">Type the price here</small>
+            </div>
+
+            <div class="mb-3">
+                <label for="description" class="form-label">description</label>
+                <input type="text" step="0.01" class="form-control" name="description" id="description"
+                    aria-describedby="helpId" placeholder="....." value="{{ $comic->description }}">
+                <small id="descriptionHelper" class="form-text text-muted">Type the description here</small>
+            </div>
+
+            <div class="mb-3">
+                <label for="series" class="form-label">series</label>
+                <input type="text" step="0.01" class="form-control" name="series" id="series"
+                    aria-describedby="helpId" placeholder="Es. Batman" value="{{ $comic->series }}">
+                <small id="seriesHelper" class="form-text text-muted">Type the series here</small>
+            </div>
+
+            <div class="mb-3">
+                <label for="type" class="form-label">type</label>
+                <input type="text" step="0.01" class="form-control" name="type" id="type"
+                    aria-describedby="helpId" placeholder="comic book etc" value="{{ $comic->type }}">
+                <small id="typeHelper" class="form-text text-muted">Type the type here</small>
+            </div>
+
+            <div class="mb-3">
+                <label for="artists" class="form-label">artists</label>
+                <input type="text" step="0.01" class="form-control" name="artists" id="artists"
+                    aria-describedby="helpId" placeholder="Clay Mann, Tony S. Daniel" value="{{ $comic->artists }}">
+                <small id="artistsHelper" class="form-text text-muted">Type the artists here</small>
+            </div>
+
+            <div class="mb-3">
+                <label for="writers" class="form-label">writers</label>
+                <input type="text" step="0.01" class="form-control" name="writers" id="writers"
+                    aria-describedby="helpId" placeholder="Scott Snyder, Dan Abnett" value="{{ $comic->writers }}">
+                <small id="writersHelper" class="form-text text-muted">Type the writers here</small>
+            </div>
+
+            <div class="mb-3">
+                <label for="sale_date" class="form-label">Sale date</label>
+                <input type="text" step="0.01" class="form-control" name="sale_date" id="sale_date"
+                    aria-describedby="helpId" placeholder="Es. 2023-10-24" value="{{ $comic->sale_date }}">
+                <small id="sale_dateHelper" class="form-text text-muted">Type the sale date here</small>
+            </div>
+
+            <div class="mb-3">
+                <label for="thumb" class="form-label">Choose file</label>
+                <input type="file" class="form-control" name="thumb" id="thumb" placeholder=""
+                    aria-describedby="thumb_helper">
+                <div id="thumb_helper" class="form-text">Upload an image for the current product</div>
+            </div>
+
+
+            <button type="submit" class="btn btn-primary">
+                Update
+            </button>
+
+
+        </form>
+
+    </div>
+@endsection
--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -1,51 +1,54 @@
 @extends('layouts.admin')
 
 @section('main')
-    <h1 class="bg-secondary py-4 text-center">
-        {{ $comic->title }}
-    </h1>
-    <div class="container">
-        <div class="row py-4">
-            <div class="col">
-                <img src="{{ $comic->thumb }}" alt="">
-            </div>
-            <div class="col">
-                <div class="py-2">
-                    <h4>
-                        Description
-                    </h4>
-                    <p>
-                        {{ $comic->description }}
-                    </p>
-                    <p class="fs-4">
-                        Series: {{ $comic->series }}
-                    </p>
-                    <p class="fs-4">
-                        Type: {{ $comic->type }}
-                    </p>
-                    <p class="fs-4">
-                        Price: {{ $comic->price }}
-                    </p>
-                    <div class="row">
-                        <div class="col">
-                            <h4>
-                                Artists
-                            </h4>
-                            <p>
-                                {{ $comic->artists }}
-                            </p>
-                        </div>
-                        <div class="col">
-                            <h4>
-                                Writers
-                            </h4>
-                            <p>
-                                {{ $comic->writers }}
-                            </p>
+    <section id="show_section">
+
+        <h1 class="bg-secondary py-4 text-center">
+            {{ $comic->title }}
+        </h1>
+        <div class="container">
+            <div class="row py-4">
+                <div class="col">
+                    <img src="{{ $comic->thumb }}" alt="">
+                </div>
+                <div class="col border-start">
+                    <div class="py-2 ps-3">
+                        <h4>
+                            Description
+                        </h4>
+                        <p>
+                            {{ $comic->description }}
+                        </p>
+                        <p class="fs-4">
+                            Series: {{ $comic->series }}
+                        </p>
+                        <p class="fs-4">
+                            Type: {{ $comic->type }}
+                        </p>
+                        <p class="fs-4">
+                            Price: {{ $comic->price }}
+                        </p>
+                        <div class="row">
+                            <div class="col">
+                                <h4>
+                                    Artists
+                                </h4>
+                                <p>
+                                    {{ $comic->artists }}
+                                </p>
+                            </div>
+                            <div class="col">
+                                <h4>
+                                    Writers
+                                </h4>
+                                <p>
+                                    {{ $comic->writers }}
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
