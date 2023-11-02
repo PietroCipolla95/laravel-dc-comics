485eb1a4a7980b9eb455afffba541c20ea9227b3
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-11-02 16:46:45 +0100

    add migration,seed,controller
    and add admin view

---
+++ app/Http/Controllers/Admin/ComicsController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comic;
+use Illuminate\Http\Request;
+
+class ComicsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $comics = Comic::all();
+        return view('admin.comics', compact('comics'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.comics.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comic $comic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Comic $comic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Comic $comic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Comic $comic)
+    {
+        //
+    }
+}
